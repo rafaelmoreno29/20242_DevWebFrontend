51e5aace85c8cfc7575e2f013f39e734134284e2
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-10-09 19:50:23 -0300

    forms

--- AulaAngular/src/app/form-template-driven/form-template-driven.component.ts
+++ AulaAngular/src/app/form-template-driven/form-template-driven.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Cliente {
+  nome: string;
+  email: string;
+  celular: string;
+}
 
 @Component({
   selector: 'app-form-template-driven',
   templateUrl: './form-template-driven.component.html',
-  styleUrls: ['./form-template-driven.component.css']
+  styleUrls: ['./form-template-driven.component.css'],
 })
 export class FormTemplateDrivenComponent {
-
+  cliente: Cliente = {
+    nome: '',
+    email: '',
+    celular: '',
+  };
+  salvar(form: NgForm) {
+    console.log(this.cliente);
+  }
 }
