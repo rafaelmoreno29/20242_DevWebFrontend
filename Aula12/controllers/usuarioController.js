88729282c3babea739a89884993a5adf4e723f9e
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-10-30 20:44:13 -0300

    aula 13

--- Aula12/controllers/usuarioController.js
+++ Aula12/controllers/usuarioController.js
@@ -1,10 +1,17 @@
+const usuarioModel = require('../models/usuario');
+
 // Lógica para obter todos os usuários
 exports.obterTodos = (req, res) => {
     res.json({ mensagem: 'Obter todos os usuários' });
 };
 
 // Lógica para criar um novo usuário
-exports.inserir = (req, res) => {
-    res.status(201).json(
-        { mensagem: 'Criar um novo usuário' });
+exports.inserir = async (req, res) => {
+    try {
+        await usuarioModel.create(req.body);
+        res.status(201).json(req.body);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+
 };
