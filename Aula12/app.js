2a83d2425b0491112a168a1846a57a042d311ff0
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-10-30 20:25:21 -0300

    aula 13

--- Aula12/app.js
+++ Aula12/app.js
@@ -2,10 +2,21 @@ const express = require('express');
 const usuarioRouter = require('./routes/usuario');
 const lista1Router = require('./routes/lista1');
 
+const mongoose = require('mongoose');
+
 const app = express();
 app.use(express.json());
 app.use('/usuario', usuarioRouter);
 app.use('/lista1', lista1Router);
 
 
-app.listen(3000, () => console.log('Servidor rodando na porta: 3000'));
+mongoose.connect('mongodb+srv://rafaelmorenosax:kpD7heAn6IEem6wC@cluster0.ugdx6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Conectado ao mongoDB');
+            console.log('Servidor iniciado na porta 3000');
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
