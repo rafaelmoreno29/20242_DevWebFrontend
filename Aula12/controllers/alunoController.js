15578bb2fec437b92d893521c808836a2e6c0114
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-10-30 22:26:09 -0300

    aula 13

--- Aula12/controllers/alunoController.js
+++ Aula12/controllers/alunoController.js
@@ -23,4 +23,17 @@ exports.obterPorRa = async (req, res) => {
         res.status(500).json({ error: error });
     }
 }
+exports.obterDisciplinasPorRa = async (req, res) => {
+    try {
+        const aluno = await alunoModel.findOne({ ra: req.params.ra });
+        if (aluno) {
+            res.status(200).json(aluno.disciplinas);
+        } else {
+            res.status(400).json({ message: 'Aluno não encontrado' });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ error: error });
+    }
+}
 
--- Aula12/routes/aluno.js
+++ Aula12/routes/aluno.js
@@ -4,5 +4,6 @@ const alunoController = require('../controllers/alunoController');
 
 router.get('/', alunoController.obterTodos);
 router.get('/:ra', alunoController.obterPorRa);
+router.get('/:ra/disciplinas', alunoController.obterDisciplinasPorRa);
 
 module.exports = router;
