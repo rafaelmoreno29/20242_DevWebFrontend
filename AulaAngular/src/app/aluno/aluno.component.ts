73d53efe31bbca4f3b73845ceacf6563c5e8095a
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-11-13 20:16:16 -0300

    Tela Aluno

--- AulaAngular/src/app/aluno/aluno.component.ts
+++ AulaAngular/src/app/aluno/aluno.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlunoService } from '../services/aluno.service';
 
 @Component({
   selector: 'app-aluno',
   templateUrl: './aluno.component.html',
   styleUrls: ['./aluno.component.css'],
 })
-export class AlunoComponent {
+export class AlunoComponent implements OnInit {
   alunos: any = [];
 
-  constructor() {}
+  constructor(private alunoService: AlunoService) {}
+
+  ngOnInit(): void {
+    this.alunoService.obterTodos().subscribe((dados: any) => {
+      this.alunos = dados;
+    });
+  }
 
   novo() {}
   editar(aluno: any) {}
