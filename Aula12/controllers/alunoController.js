3faa4e45562a93f68572c829ae3802044ab9511e
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-11-27 19:11:37 -0300

    acerta apo

--- Aula12/controllers/alunoController.js
+++ Aula12/controllers/alunoController.js
@@ -10,6 +10,47 @@ exports.obterTodos = async (req, res) => {
     }
 };
 
+exports.remover = async (req, res) => {
+    try {
+        const aluno = await alunoModel.findOne({ _id: req.params.id });
+        if (aluno) {
+            await alunoModel.deleteOne({ _id: req.params.id });
+            res.status(200).json({ message: 'Aluno removido com sucesso' });
+        } else {
+            res.status(400).json({ message: 'Aluno não encontrado' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+}
+exports.editar = async (req, res) => {
+    try {
+        const aluno = await alunoModel.findOne({ _id: req.params.id });
+        if (aluno) {
+            await alunoModel.updateOne({ _id: req.params.id }, req.body);
+            res.status(200).json(req.body);
+        } else {
+            res.status(400).json({ message: 'Aluno não encontrado' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+}
+
+exports.obterPorId = async (req, res) => {
+    try {
+        const aluno = await alunoModel.findOne({ _id: req.params.id });
+        if (aluno) {
+            res.status(200).json(aluno);
+        } else {
+            res.status(400).json({ message: 'Aluno não encontrado' });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ error: error });
+    }
+}
+
 exports.obterPorRa = async (req, res) => {
     try {
         const aluno = await alunoModel.findOne({ ra: req.params.ra });
@@ -37,3 +78,12 @@ exports.obterDisciplinasPorRa = async (req, res) => {
     }
 }
 
+exports.inserir = async (req, res) => {
+    try {
+        await alunoModel.create(req.body);
+        res.status(201).json(req.body);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+
+};
--- Aula12/routes/aluno.js
+++ Aula12/routes/aluno.js
@@ -5,5 +5,8 @@ const alunoController = require('../controllers/alunoController');
 router.get('/', alunoController.obterTodos);
 router.get('/:ra', alunoController.obterPorRa);
 router.get('/:ra/disciplinas', alunoController.obterDisciplinasPorRa);
+router.post('/', alunoController.inserir);
+router.put('/:id', alunoController.editar);
+router.delete('/:id', alunoController.remover);
 
 module.exports = router;
